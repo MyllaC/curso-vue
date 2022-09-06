857cd6ef5e8e91b211980be24eabefda9a99253f
Camila <criscamila09@gmail.com>
2022-09-06 17:56:45 -0300

    aula14_eventos-submut-click

--- 2_projeto_vue/src/components/form.vue
+++ 2_projeto_vue/src/components/form.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
+  <div class="form">
     <h2>Meu formulário</h2>
-    <form action="">
-      <div><InputText /></div>
-      <div><InputText /></div>
+    <form action="" @submit="enviarFormulario($event)">
+      <!--
+        <div><InputText /></div>
+        <div><InputText /></div>
+      -->
+      <div>
+        <input type="text" v-model="name" />
+      </div>
+      <div>
+        <input type="text" v-model="email" />
+      </div>
       <div><Submit /></div>
     </form>
   </div>
@@ -15,5 +23,30 @@ import Submit from "./form/Submit.vue";
 export default {
   name: "Form",
   components: { InputText, Submit },
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  methods: {
+    enviarFormulario(e) {
+      e.preventDefault();
+
+      const name = this.name;
+      const email = this.email;
+
+      console.log("Formulario enviado");
+      console.log(`Nome: ${name} Email: ${email}`);
+
+      //Ajax para mandar os dados para o banco de dados
+    },
+  },
 };
 </script>
+
+<style scoped>
+.form {
+  padding-bottom: 100px;
+}
+</style>
