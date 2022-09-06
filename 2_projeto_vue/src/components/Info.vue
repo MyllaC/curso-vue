72ca74a11d6242dc3df62f11a93a92a91892e6b8
Camila <criscamila09@gmail.com>
2022-09-06 16:01:12 -0300

    aula08_hierarquia-de-componentes

---
+++ 2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Estou trabalhando no momento</p>
+    <p>Utilizado as seguintes tecnologias</p>
+    <ul>
+      <li>JavaScript</li>
+      <li>Java</li>
+      <li>React</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Info",
+};
+</script>
