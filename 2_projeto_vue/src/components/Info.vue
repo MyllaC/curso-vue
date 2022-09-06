3c3d50f0f2b46cbe8ee5ad4213db995af14ca67d
Camila <criscamila09@gmail.com>
2022-09-06 16:18:43 -0300

    aula09_diretivas-vif-vshow

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utilizado as seguintes tecnologias</p>
     <ul>
       <li>JavaScript</li>
       <li>Java</li>
       <li>React</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Info",
+  data() {
+    return {
+      esta_trabalhando: false,
+      mostrar_email: false,
+      email: "criscamila09@gmail.com",
+    };
+  },
 };
 </script>
