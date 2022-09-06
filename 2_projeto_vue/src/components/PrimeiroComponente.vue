863a272fedb6ed6150432cd6dda59eab0196a42f
Camila <criscamila09@gmail.com>
2022-09-06 14:43:01 -0300

    aula06_dados-em-componentes

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
+  <div>
     <h1>Olá Vue</h1>
+    <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'PrimeiroComponente'
-    }
+export default {
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      nome: "Camila",
+      profissao: "Programadora",
+    };
+  },
+};
 </script>
